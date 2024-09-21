5b178b0343074e8b422d0c004fb7e571249a6ddf
Himanshu-AT <aristech.official@gmail.com>
2024-09-21 16:20:27 +0530

    db connection working

--- core/db-handler/src/lib.rs
+++ core/db-handler/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use prisma;
+use tokio;
+
+async fn connect_to_pg(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let _pg_pool = prisma::connect_to_pg(url).await?;
+    Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn connect_to_mysql(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let _mysql_pool = prisma::connect_to_mysql(url).await?;
+    Ok(())
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+async fn connect_to_mongodb(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let _mongodb_client = prisma::connect_to_mongodb(url).await?;
+    Ok(())
+}
+
+pub async fn connect_db(db: &str, url: &str) {
+    match db {
+        "pg" => match connect_to_pg(url).await {
+            Ok(_) => println!("Successfully connected to the PostgreSQL database."),
+            Err(e) => println!("Failed to connect to the PostgreSQL database: {}", e),
+        },
+        "mysql" => match connect_to_mysql(url).await {
+            Ok(_) => println!("Successfully connected to the MySQL database."),
+            Err(e) => println!("Failed to connect to the MySQL database: {}", e),
+        },
+        "mg" => match connect_to_mongodb(url).await {
+            Ok(_) => println!("Successfully connected to the MongoDB database."),
+            Err(e) => println!("Failed to connect to the MongoDB database: {}", e),
+        },
+        _ => println!("Invalid database type."),
     }
 }
--- core/prisma/src/lib.rs
+++ core/prisma/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use mongodb::{options::ClientOptions, Client};
+use sqlx::{MySqlPool, PgPool};
+use std::error::Error;
+
+pub async fn connect_to_pg(url: &str) -> Result<PgPool, Box<dyn Error>> {
+    let pool = PgPool::connect(url).await?;
+    Ok(pool)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn connect_to_mysql(url: &str) -> Result<MySqlPool, Box<dyn Error>> {
+    let pool = MySqlPool::connect(url).await?;
+    Ok(pool)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub async fn connect_to_mongodb(url: &str) -> Result<Client, Box<dyn Error>> {
+    let mut client_options = ClientOptions::parse(url).await?;
+    client_options.app_name = Some("My App".to_string());
+    let client = Client::with_options(client_options)?;
+    Ok(client)
 }
